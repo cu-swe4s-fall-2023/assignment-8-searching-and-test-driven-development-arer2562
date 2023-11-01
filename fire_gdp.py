32f33557dc60235771aad379dfe5275beb6ff9f9
arer2562 <jovyan@jupyter-arer2562>
2023-11-01 23:57:22 +0000

    Changes to be committed: 	modified:   fire_gdp.py 	modified:   test_fire_gdp.py

--- fire_gdp.py
+++ fire_gdp.py
@@ -3,4 +3,10 @@ import csv
 def clean_str(string):
     return string.replace(",", "")
 
+def search(L, k):
+    for i, value in enumerate(L):
+        if k == value:
+            return i
+    return None
+
 
--- test_fire_gdp.py
+++ test_fire_gdp.py
@@ -6,7 +6,24 @@ class TestFireGDP(unittest.TestCase):
     def test_clean_str(self):
         self.assertEqual(fire_gdp.clean_str('1,000'), '1000')
         self.assertEqual(fire_gdp.clean_str('1,000,000'), '1000000')
-        
+    
+    def test_search(self):
+        self.assertEqual(fire_gdp.search([1,2,3,4,5], 3), 2)
+        self.assertEqual(fire_gdp.search([1,2,3,4,5], 100), None)
+        self.assertEqual(fire_gdp.search([1,2,3,3,4,5], 3), 2)
+        self.assertEqual(fire_gdp.search([3,1,2,3,4,5], 3), 0)
+        self.assertEqual(fire_gdp.search([], 3), None)
+
+        for i in range(100):
+            random_list = [random.randint(1,100000) for x in range(1000)]
+            random_value = random.choice(random_list)
+            first_value = random_list[0]
+            no_hit_value = 1000000
+            self.assertIsNotNone(fire_gdp.search(random_list, random_value))
+            self.assertIsNone(fire_gdp.search(random_list, no_hit_value))
+            self.assertEqual(fire_gdp.search(random_list,first_value), 0)
+
+    
         
         
 if __name__ == '__main__':
