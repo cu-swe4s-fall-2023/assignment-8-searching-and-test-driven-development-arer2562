ac51ec341beac4bff76dccd2cbbd46172a20a31c
arer2562 <jovyan@jupyter-arer2562>
2023-11-02 00:09:41 +0000

    On branch Assn8  Changes to be committed: 	modified:   test_fire_gdp.py

--- test_fire_gdp.py
+++ test_fire_gdp.py
@@ -34,8 +34,60 @@ class TestFireGDP(unittest.TestCase):
         
         self.assertEqual(fire_gdp.get_data(empty_file_name), [])
         self.assertRaises(FileNotFoundError, fire_gdp.get_data, dne_file_name)
-            
+    
+    def test_get_data_read_lines(self):
+        fire_file_name = 'Agrofood_co2_emission.csv'
+        gdp_file_name = 'IMF_GDP.csv'
+        empty_file_name = 'empty.txt'
 
+        self.assertEqual(len(fire_gdp.get_data(fire_file_name)), 6966)
+        self.assertEqual(len(fire_gdp.get_data(gdp_file_name)), 199)
+        self.assertEqual(len(fire_gdp.get_data(empty_file_name)), 0)
+            
+    def test_get_get_header(self):
+        fire_file_name = 'Agrofood_co2_emission.csv'
+        gdp_file_name = 'IMF_GDP.csv'
+        empty_file_name = 'empty.txt'
+        
+        target_country = 'Zimbabwe'
+        
+        data, header = fire_gdp.get_data(fire_file_name, 
+                                         query_value=target_country, 
+                                         query_col=0,
+                                         get_header=True)
+        self.assertEqual(len(data), 31)
+        self.assertEqual(len(header), 31)
+        
+        data,header = fire_gdp.get_data(gdp_file_name,
+                                        query_value=target_country,
+                                        query_col=0,
+                                        get_header=True)
+        
+        self.assertEqual(len(data), 1)
+        self.assertEqual(len(header), 74)
+        
+        data, header = fire_gdp.get_data(empty_file_name,
+                                         query_value=target_country,
+                                         query_col=0,
+                                         get_header=True)
+         
+        self.assertEqual(len(data), 0)
+        self.assertEqual(len(header), 0)       
+        
+        data, header = fire_gdp.get_data(gdp_file_name,
+                                         query_value=target_country,
+                                         query_col=1,
+                                         get_header=True) 
+        
+        self.assertEqual(len(data), 0)
+        self.assertEqual(len(header), 74)
+        
+        self.assertRaises(IndexError,
+                          fire_gdp.get_data,
+                          gdp_file_name,
+                          query_value=target_country,
+                          query_col=10000,
+                          get_header=True)
     
         
         
